9f6f52c987caaf22abe13f28f2824fcbd8e8959f
Dowland Aiello <mitsukomegumii@gmail.com>
2019-02-16 19:04:24 -0500

    Sync....

--- src/main/java/com/dowlandaiello/gitchain/types/Transaction.java
+++ src/main/java/com/dowlandaiello/gitchain/types/Transaction.java
@@ -1,6 +1,9 @@
 package com.dowlandaiello.gitchain.types;
 
+import java.io.Serializable;
 import java.math.BigInteger;
+
+import org.apache.commons.lang3.SerializationUtils;
 
 /**
  * Transaction is a data alteration represented by the transaction primitive (e.g. updating a file), that
@@ -9,7 +12,10 @@ import java.math.BigInteger;
  * @author Dowland Aiello
  * @since 15.02.2019
  */
-public class Transaction {
+public class Transaction implements Serializable {
+    /* lol serialization */
+    static final long serialVersionUID = 0L;
+
     /* Transaction index in account tx list */
     public int AccountNonce;
 
@@ -20,7 +26,7 @@ public class Transaction {
     public byte[] Recipient;
 
     /* Transaction signature expressed as a signature type primitive */
-    public byte[] Signature; // TODO: Add signature type primitive
+    public Signature Signature;
 
     /* Transaction amount expressed as a float */
     public float Value;
@@ -34,12 +40,12 @@ public class Transaction {
     /**
      * Initialize a new transaction.
      * 
-     * @param nonce
-     * @param sender
-     * @param recipient
-     * @param value
-     * @param operation
-     * @param payload
+     * @param nonce transaction index in account tx set
+     * @param sender transaction sender expressed as byte array
+     * @param recipient transaction recipient expressed as byte array
+     * @param value transaction value
+     * @param operation given tx operation
+     * @param payload transaction asm bytecodes
      */
     public Transaction(int nonce, byte[] sender, byte[] recipient, float value, int operation, byte[] payload) {
         this.AccountNonce = nonce; // Set nonce
@@ -51,11 +57,43 @@ public class Transaction {
     }
 
     /**
+     * Decode a transaction from a given raw data set, rawData.
+     * 
+     * @param rawData data set to decode
+     */
+    public Transaction(byte[] rawData) {
+        Transaction decodedTx = SerializationUtils.deserialize(rawData); // Deserialize
+
+        this.AccountNonce = decodedTx.AccountNonce; // Set nonce
+        this.Sender = decodedTx.Sender; // Set sender
+        this.Recipient = decodedTx.Recipient; // Set recipient
+        this.Signature = decodedTx.Signature; // Set signature
+        this.Value = decodedTx.Value; // Set value
+        this.Operation = decodedTx.Operation; // Set operation
+        this.Payload = decodedTx.Payload; // Set payload
+    }
+
+    /**
      * Sign a given transaction.
      * 
      * @param privateKey the given private key to sign with
+     * @return whether the transaction was signed successfully
      */
-    public void Sign(BigInteger privateKey) {
-        // TODO: do this
+    public boolean Sign(BigInteger privateKey) {
+        if (this.Bytes() == null) {
+            return false; // Nothing to sign ¯\_(ツ)_/¯
+        }
+        this.Signature = new Signature(this, privateKey); // Sign transaction
+
+        return true; // Return success
+    }
+
+    /**
+     * Serialize the working transaction to a byte array.
+     * 
+     * @return the serialized transaction
+     */
+    public byte[] Bytes() {
+        return(SerializationUtils.serialize(this)); // Serialize
     }
 }
