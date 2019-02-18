3024a4468dae074bf2cd4b4bb9a9a04d04635618
Dowland Aiello <mitsukomegumii@gmail.com>
2019-02-18 13:22:16 -0500

    Sync...

--- src/main/java/com/dowlandaiello/gitchain/types/Blockchain.java
+++ src/main/java/com/dowlandaiello/gitchain/types/Blockchain.java
@@ -2,6 +2,8 @@ package com.dowlandaiello.gitchain.types;
 
 import java.util.ArrayList;
 
+import com.dowlandaiello.gitchain.config.ChainConfig;
+
 /**
  * Blockchain is a data type containing a list of blocks, a genesis block, and consensus metadata.
  * 
@@ -12,7 +14,18 @@ public class Blockchain {
     /* Blocks in blockchain */
     public ArrayList<Block> Blocks;
 
-    public Blockchain() {
+    /* Chain alloc, configuration */
+    public ChainConfig Config;
 
+    /* Blockchain ID / version */
+    public String ChainID;
+
+    /* Network ID */
+    public int Network;
+
+    public Blockchain(ChainConfig chainConfig) {
+        this.Config = chainConfig; // Set chain config
+        this.ChainID = chainConfig.Chain; // Set chain name
+        this.Network = chainConfig.Network; // Set network id
     }
 }
