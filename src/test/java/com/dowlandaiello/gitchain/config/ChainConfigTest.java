7d333a8c05adea8b4fd7d2fb4b821830cd7ec01e
Dowland Aiello <mitsukomegumii@gmail.com>
2019-02-18 01:59:48 -0500

    Not working sad ree.

--- src/test/java/com/dowlandaiello/gitchain/config/ChainConfigTest.java
+++ src/test/java/com/dowlandaiello/gitchain/config/ChainConfigTest.java
@@ -3,8 +3,11 @@ package com.dowlandaiello.gitchain.config;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.dowlandaiello.gitchain.common.CommonIO;
 
 import org.junit.Test;
 import org.web3j.crypto.ECKeyPair;
@@ -37,5 +40,43 @@ public class ChainConfigTest {
         ChainConfig chainConfig = new ChainConfig(alloc, 0, "test_chain"); // Initialize chain config
 
         assertTrue("chain config must not be null", chainConfig != null); // Ensure config not null
+    }
+
+    /**
+     * Test genesis.json read helper.
+     */
+    @Test
+    public void TestReadChainConfig() {
+        CommonIO.GenesisPath = new File(CommonIO.ConfigPath+"/genesis_test.json").getAbsolutePath(); // Set genesis path
+
+        ChainConfig chainConfig = ChainConfig.ReadChainConfig(); // Read chain config
+
+        assertTrue("chain config must not be null", chainConfig != null); // Ensure config not null
+    }
+
+    /**
+     * Test chain config constructor.
+     */
+    @Test
+    public void TestWriteChainConfig() {
+        ECKeyPair keyPair = null; // Init buffer
+
+        try {
+            keyPair = Keys.createEcKeyPair(); // Generate a new key pair
+        } catch (Exception e) { // Catch
+            fail(e.getLocalizedMessage()); // Panic
+        }
+
+        CommonIO.GenesisPath = new File(CommonIO.ConfigPath+"/genesis_test.json").getAbsolutePath(); // Set genesis path
+
+        Map<byte[], Float> alloc = new HashMap<byte[], Float>(); // Init hash map
+
+        alloc.put(keyPair.getPublicKey().toByteArray(), 1000000f); // Set alloc
+
+        ChainConfig chainConfig = new ChainConfig(alloc, 0, "test_chain"); // Initialize chain config
+
+        assertTrue("chain config must not be null", chainConfig != null); // Ensure config not null
+
+        assertTrue("must write successfully", chainConfig.WriteChainConfig()); // Ensure success
     }
 }
