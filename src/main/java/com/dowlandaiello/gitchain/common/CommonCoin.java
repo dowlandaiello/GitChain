1dbfe8c0ceb5c2edb1abd7263fe7b0934f561aa3
Dowland Aiello <mitsukomegumii@gmail.com>
2019-02-18 16:17:25 -0500

    Added commonCoin defs.

---
+++ src/main/java/com/dowlandaiello/gitchain/common/CommonCoin.java
@@ -0,0 +1,12 @@
+package com.dowlandaiello.gitchain.common;
+
+/**
+ * CommonCoin is a common coin param placeholder.
+ * 
+ * @author Dowland Aiello
+ * @since 18.02.2019
+ */
+public class CommonCoin {
+    /* Miner coinbase */
+    public static byte[] MinerCoinbase;
+}
