8cf0a63a4ffe24d5a1797a05815180ce66ed6b97
Dowland Aiello <mitsukomegumii@gmail.com>
2019-02-19 14:29:52 -0500

    Created CommonByteCmp helper class.

---
+++ src/main/java/com/dowlandaiello/gitchain/common/CommonByteCmp.java
@@ -0,0 +1,11 @@
+package com.dowlandaiello.gitchain.common;
+
+/**
+ * CommonByteCmp is a class for common byte comparison operations.
+ * 
+ * @author Dowland Aiello
+ * @since 19.02.2019
+ */
+public class CommonByteCmp {
+
+}
