5c81fa4437b919b061080c78661f080969fa1cca
Dowland Aiello <mitsukomegumii@gmail.com>
2019-02-21 13:31:02 -0500

    Created CommonNet.java networking params and defs.

---
+++ src/main/java/com/dowlandaiello/gitchain/common/CommonNet.java
@@ -0,0 +1,14 @@
+package com.dowlandaiello.gitchain.common;
+
+/**
+ * CommonNet outlines common networking definitions and parameters.
+ */
+public class CommonNet {
+    /* Default node port */
+    public static long NodePort = 3000;
+
+    /* Common IP protocols */
+    public enum Protocol {
+        IPV4, IPV6; // Protocol defs
+    }
+}
