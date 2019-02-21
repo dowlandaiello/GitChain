7eef1a52f32f1e4e1ebabd43eec9944858a5dc55
Dowland Aiello <mitsukomegumii@gmail.com>
2019-02-21 00:08:59 -0500

    Formatted.

--- src/test/java/com/dowlandaiello/gitchain/types/BlockchainTest.java
+++ src/test/java/com/dowlandaiello/gitchain/types/BlockchainTest.java
@@ -3,7 +3,6 @@ package com.dowlandaiello.gitchain.types;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.io.IOException;
 import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,7 +12,6 @@ import com.dowlandaiello.gitchain.common.CommonIO;
 import com.dowlandaiello.gitchain.config.ChainConfig;
 import com.dowlandaiello.gitchain.crypto.Sha;
 
-import org.iq80.leveldb.DBIterator;
 import org.junit.Test;
 import org.web3j.crypto.ECKeyPair;
 import org.web3j.crypto.Keys;
@@ -44,29 +42,26 @@ public class BlockchainTest {
 
         ChainConfig chainConfig = new ChainConfig(alloc, 0, "test_chain", 10, 1f); // Initialize chain config
 
-        assertTrue("must remove test cache", CommonIO.DeleteDirectoryContents(new java.io.File(CommonIO.DbPath + "/" + chainConfig.Chain))); // Delete test cache
+        assertTrue("must remove test cache",
+                CommonIO.DeleteDirectoryContents(new java.io.File(CommonIO.DbPath + "/" + chainConfig.Chain))); // Delete
+                                                                                                                // test
+                                                                                                                // cache
 
         assertTrue("chain config must not be null", chainConfig != null); // Ensure config not null
 
         Blockchain blockchain = new Blockchain(chainConfig); // Make new blockchain
 
-        DBIterator iterator = blockchain.BlockDB.iterator(); // Get iterator
-
-        iterator.seekToFirst(); // Seek to first element
-
         assertTrue("blockchain must not be null", blockchain != null); // Ensure chain not null
         assertTrue("genesis block must not be null", blockchain.GenesisBlock != null); // Ensure genesis not null
-        assertTrue("genesis block in blocks must not be null", iterator.peekNext().getValue() != null); // Ensure genesis not null
-
-        try {
-            iterator.close(); // Close iterator
-        } catch (IOException e) {
-            fail(e.getLocalizedMessage()); // Panic
-        }
+        assertTrue("genesis block in blocks must not be null", blockchain.GetLastBlock() != null); // Ensure genesis not
+                                                                                                   // null
 
         blockchain.CloseBlockDB(); // Close block db
 
-        assertTrue("must remove test cache", CommonIO.DeleteDirectoryContents(new java.io.File(CommonIO.DbPath + "/" + chainConfig.Chain))); // Delete test cache
+        assertTrue("must remove test cache",
+                CommonIO.DeleteDirectoryContents(new java.io.File(CommonIO.DbPath + "/" + chainConfig.Chain))); // Delete
+                                                                                                                // test
+                                                                                                                // cache
     }
 
     /**
@@ -88,31 +83,30 @@ public class BlockchainTest {
 
         ChainConfig chainConfig = new ChainConfig(alloc, 0, "test_chain", 13, 255.9f); // Initialize chain config
 
-        assertTrue("must remove test cache", CommonIO.DeleteDirectoryContents(new java.io.File(CommonIO.DbPath + "/" + chainConfig.Chain))); // Delete test cache
+        assertTrue("must remove test cache",
+                CommonIO.DeleteDirectoryContents(new java.io.File(CommonIO.DbPath + "/" + chainConfig.Chain))); // Delete
+                                                                                                                // test
+                                                                                                                // cache
 
         assertTrue("chain config must not be null", chainConfig != null); // Ensure config not null
 
         long genesisStartingTime = System.currentTimeMillis() / 1000; // Get current timestamp
 
-        System.out.println(genesisStartingTime + ": making genesis block (this shouldn't take more than 25 seconds)...\n"); // Log gen time
+        System.out.println(
+                genesisStartingTime + ": making genesis block (this shouldn't take more than 25 seconds)...\n"); // Log
+                                                                                                                 // gen
+                                                                                                                 // time
 
         Blockchain blockchain = new Blockchain(chainConfig); // Make new blockchain
 
-        System.out.println(System.currentTimeMillis() / 1000 + ": finished making genesis block with difficulty " + blockchain.GenesisBlock.Difficulty + ", nonce " + blockchain.GenesisBlock.Nonce + " and block time " + (blockchain.GenesisBlock.Timestamp - genesisStartingTime)); // Log start time
-
-        DBIterator iterator = blockchain.BlockDB.iterator(); // Get iterator
-
-        iterator.seekToFirst(); // Seek to first element
+        System.out.println(System.currentTimeMillis() / 1000 + ": finished making genesis block with difficulty "
+                + blockchain.GenesisBlock.Difficulty + ", nonce " + blockchain.GenesisBlock.Nonce + " and block time "
+                + (blockchain.GenesisBlock.Timestamp - genesisStartingTime)); // Log start time
 
         assertTrue("blockchain must not be null", blockchain != null); // Ensure chain not null
         assertTrue("genesis block must not be null", blockchain.GenesisBlock != null); // Ensure genesis not null
-        assertTrue("genesis block in blocks must not be null", iterator.peekNext().getValue() != null); // Ensure genesis not null
-
-        try {
-            iterator.close(); // Close iterator
-        } catch (IOException e) {
-            fail(e.getLocalizedMessage()); // Panic
-        }
+        assertTrue("genesis block in blocks must not be null", blockchain.GetLastBlock() != null); // Ensure genesis not
+                                                                                                   // null
 
         int numBlocks = 3; // Number of blocks to make
 
@@ -128,33 +122,40 @@ public class BlockchainTest {
 
             Block newBlock = blockchain.CreateNewBlock(lastBlock, transactions, 0); // Generate new block
             newBlock.Difficulty = Blockchain.CalculateDifficulty(lastBlock, newBlock.Timestamp); // Set difficulty
-    
+
             assertTrue("block must not be null", newBlock != null); // Ensure block not null
-    
+
             while (!Blockchain.VerifyBlockNonce(newBlock)) { // Check invalid hash
                 newBlock.Nonce++; // Increment nonce
                 newBlock.Timestamp = System.currentTimeMillis() / 1000; // Set timestamp
                 newBlock.Difficulty = Blockchain.CalculateDifficulty(lastBlock, newBlock.Timestamp); // Set difficulty
             }
-    
+
             newBlock.Hash = Sha.Sha3(newBlock.BytesHashSafe()); // Hash
 
             totalDifficulty += newBlock.Difficulty; // Add difficulty
             totalNonce += newBlock.Nonce; // Add nonce
             totalBlockTime += (newBlock.Timestamp - lastBlock.Timestamp); // Add timestamp
-    
-            System.out.println(System.currentTimeMillis() / 1000 + ": finished making block "+x+" with difficulty " + newBlock.Difficulty + ", nonce " + newBlock.Nonce + ", and block time " + (newBlock.Timestamp - lastBlock.Timestamp)); // Log finish time
+
+            System.out.println(System.currentTimeMillis() / 1000 + ": finished making block " + x + " with difficulty "
+                    + newBlock.Difficulty + ", nonce " + newBlock.Nonce + ", and block time "
+                    + (newBlock.Timestamp - lastBlock.Timestamp)); // Log finish time
 
             lastBlock = newBlock; // Set last block
         }
 
-        blockchain.CloseBlockDB(); // Close block db
+        assertTrue("must remove test cache",
+                CommonIO.DeleteDirectoryContents(new java.io.File(CommonIO.DbPath + "/" + chainConfig.Chain))); // Delete
+                                                                                                                // test
+                                                                                                                // cache
 
-        assertTrue("must remove test cache", CommonIO.DeleteDirectoryContents(new java.io.File(CommonIO.DbPath + "/" + chainConfig.Chain))); // Delete test cache
-
-        System.out.println("\nfinished making " + numBlocks + " blocks with an average difficulty of " + totalDifficulty / numBlocks + ", an average nonce of "+ totalNonce / numBlocks +", and an average block time of "+ totalBlockTime / numBlocks +" in " + (System.currentTimeMillis() / 1000 - blockchain.GenesisBlock.Timestamp) +" seconds."); // Log test finished
+        System.out.println("\nfinished making " + numBlocks + " blocks with an average difficulty of "
+                + totalDifficulty / numBlocks + ", an average nonce of " + totalNonce / numBlocks
+                + ", and an average block time of " + totalBlockTime / numBlocks + " in "
+                + (System.currentTimeMillis() / 1000 - blockchain.GenesisBlock.Timestamp) + " seconds."); // Log test
+                                                                                                          // finished
     }
-    
+
     /**
      * Test block append helper.
      */
@@ -174,31 +175,30 @@ public class BlockchainTest {
 
         ChainConfig chainConfig = new ChainConfig(alloc, 0, "test_chain", 13, 255.9f); // Initialize chain config
 
-        assertTrue("must remove test cache", CommonIO.DeleteDirectoryContents(new java.io.File(CommonIO.DbPath + "/" + chainConfig.Chain))); // Delete test cache
+        assertTrue("must remove test cache",
+                CommonIO.DeleteDirectoryContents(new java.io.File(CommonIO.DbPath + "/" + chainConfig.Chain))); // Delete
+                                                                                                                // test
+                                                                                                                // cache
 
         assertTrue("chain config must not be null", chainConfig != null); // Ensure config not null
 
         long genesisStartingTime = System.currentTimeMillis() / 1000; // Get current timestamp
 
-        System.out.println(genesisStartingTime + ": making genesis block (this shouldn't take more than 25 seconds)...\n"); // Log gen time
+        System.out.println(
+                genesisStartingTime + ": making genesis block (this shouldn't take more than 25 seconds)...\n"); // Log
+                                                                                                                 // gen
+                                                                                                                 // time
 
         Blockchain blockchain = new Blockchain(chainConfig); // Make new blockchain
 
-        System.out.println(System.currentTimeMillis() / 1000 + ": finished making genesis block with difficulty " + blockchain.GenesisBlock.Difficulty + ", nonce " + blockchain.GenesisBlock.Nonce + " and block time " + (blockchain.GenesisBlock.Timestamp - genesisStartingTime)); // Log start time
-
-        DBIterator iterator = blockchain.BlockDB.iterator(); // Get iterator
-
-        iterator.seekToFirst(); // Seek to first element
+        System.out.println(System.currentTimeMillis() / 1000 + ": finished making genesis block with difficulty "
+                + blockchain.GenesisBlock.Difficulty + ", nonce " + blockchain.GenesisBlock.Nonce + " and block time "
+                + (blockchain.GenesisBlock.Timestamp - genesisStartingTime)); // Log start time
 
         assertTrue("blockchain must not be null", blockchain != null); // Ensure chain not null
         assertTrue("genesis block must not be null", blockchain.GenesisBlock != null); // Ensure genesis not null
-        assertTrue("genesis block in blocks must not be null", iterator.peekNext().getValue() != null); // Ensure genesis not null
-
-        try {
-            iterator.close(); // Close iterator
-        } catch (IOException e) {
-            fail(e.getLocalizedMessage()); // Panic
-        }
+        assertTrue("genesis block in blocks must not be null", blockchain.GetLastBlock() != null); // Ensure genesis not
+                                                                                                   // null
 
         int numBlocks = 3; // Number of blocks to make
 
@@ -214,15 +214,15 @@ public class BlockchainTest {
 
             Block newBlock = blockchain.CreateNewBlock(lastBlock, transactions, 0); // Generate new block
             newBlock.Difficulty = Blockchain.CalculateDifficulty(lastBlock, newBlock.Timestamp); // Set difficulty
-    
+
             assertTrue("block must not be null", newBlock != null); // Ensure block not null
-    
+
             while (!Blockchain.VerifyBlockNonce(newBlock)) { // Check invalid hash
                 newBlock.Nonce++; // Increment nonce
                 newBlock.Timestamp = System.currentTimeMillis() / 1000; // Set timestamp
                 newBlock.Difficulty = Blockchain.CalculateDifficulty(lastBlock, newBlock.Timestamp); // Set difficulty
             }
-    
+
             newBlock.Hash = Sha.Sha3(newBlock.BytesHashSafe()); // Hash
 
             totalDifficulty += newBlock.Difficulty; // Add difficulty
@@ -230,16 +230,25 @@ public class BlockchainTest {
             totalBlockTime += (newBlock.Timestamp - lastBlock.Timestamp); // Add timestamp
 
             assertTrue("must add to chain", blockchain.AddBlock(newBlock)); // Add new block to chain
-    
-            System.out.println(System.currentTimeMillis() / 1000 + ": finished making block "+x+" with difficulty " + newBlock.Difficulty + ", nonce " + newBlock.Nonce + ", and block time " + (newBlock.Timestamp - lastBlock.Timestamp)); // Log finish time
+
+            System.out.println(System.currentTimeMillis() / 1000 + ": finished making block " + x + " with difficulty "
+                    + newBlock.Difficulty + ", nonce " + newBlock.Nonce + ", and block time "
+                    + (newBlock.Timestamp - lastBlock.Timestamp)); // Log finish time
 
             lastBlock = newBlock; // Set last block
         }
 
         blockchain.CloseBlockDB(); // Close block db
 
-        assertTrue("must remove test cache", CommonIO.DeleteDirectoryContents(new java.io.File(CommonIO.DbPath + "/" + chainConfig.Chain))); // Delete test cache
+        assertTrue("must remove test cache",
+                CommonIO.DeleteDirectoryContents(new java.io.File(CommonIO.DbPath + "/" + chainConfig.Chain))); // Delete
+                                                                                                                // test
+                                                                                                                // cache
 
-        System.out.println("\nfinished making " + numBlocks + " blocks with an average difficulty of " + totalDifficulty / numBlocks + ", an average nonce of "+ totalNonce / numBlocks +", and an average block time of "+ totalBlockTime / numBlocks +" in " + (System.currentTimeMillis() / 1000 - blockchain.GenesisBlock.Timestamp) +" seconds."); // Log test finished
+        System.out.println("\nfinished making " + numBlocks + " blocks with an average difficulty of "
+                + totalDifficulty / numBlocks + ", an average nonce of " + totalNonce / numBlocks
+                + ", and an average block time of " + totalBlockTime / numBlocks + " in "
+                + (System.currentTimeMillis() / 1000 - blockchain.GenesisBlock.Timestamp) + " seconds."); // Log test
+                                                                                                          // finished
     }
 }
