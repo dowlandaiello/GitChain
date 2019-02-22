acd436a00354a6aad4fc4a82c4eedf3d25acf4af
Dowland Aiello <mitsukomegumii@gmail.com>
2019-02-21 23:20:16 -0500

    Made peer serializable.

--- src/main/java/com/dowlandaiello/gitchain/p2p/Peer.java
+++ src/main/java/com/dowlandaiello/gitchain/p2p/Peer.java
@@ -1,14 +1,21 @@
 package com.dowlandaiello.gitchain.p2p;
 
+import java.io.Serializable;
 import java.math.BigInteger;
 
 import com.dowlandaiello.gitchain.account.Account;
+import com.dowlandaiello.gitchain.common.CommonIO;
 import com.dowlandaiello.gitchain.common.CommonNet;
+
+import org.apache.commons.lang3.SerializationUtils;
 
 /**
  * Peer is a data type representing a node's public identity on a network.
  */
-public class Peer {
+public class Peer implements Serializable {
+    /* lol serialization */
+    static final long serialVersionUID = CommonIO.SerialVersionUID;
+
     /* Peer public key used for peer identification (separate from coin public key) */
     public BigInteger PublicKey;
 
@@ -20,6 +27,8 @@ public class Peer {
 
     /**
      * Initialize a new peer with a given connection address, generating a new key pair for the peer.
+     * 
+     * @param connectionAddr address used for connecting to node
      */
     public Peer(String connectionAddr) {
         Account account = new Account(); // Initialize account
@@ -38,6 +47,9 @@ public class Peer {
 
     /**
      * Initialize a new peer with a given connection address and elliptic curve identity.
+     * 
+     * @param connectionAddr address used for connecting to node
+     * @param publicKey bigInteger address used for identifying node
      */
     public Peer(String connectionAddr, BigInteger publicKey) {
         this.PublicKey = publicKey; // Set public key
@@ -52,10 +64,36 @@ public class Peer {
 
     /**
      * Initialize a new peer with a given connection address, identity, and protocol.
+     * 
+     * @param connectionAddr address used for connecting to node
+     * @param publicKey bigInteger address used for identifying node
+     * @param protocol active IP protocol (v4 or v6)
      */
     public Peer(String connectionAddr, BigInteger publicKey, CommonNet.Protocol protocol) {
         this.PublicKey = publicKey; // Set public key
-        this.ConnectionAddr = connectionAddr; // Set connection address
         this.Protocol = protocol; // Set protocol
+        this.ConnectionAddr = connectionAddr; // Set connection address
+    }
+
+    /**
+     * Deserialize a peer from given raw data, rawByte.
+     * 
+     * @param rawBytes raw data to deserialize
+     */
+    public Peer(byte[] rawBytes) {
+        Peer peer = SerializationUtils.deserialize(rawBytes); // Deserialize
+
+        this.PublicKey = peer.PublicKey; // Set public key
+        this.Protocol = peer.Protocol; // Set protocol
+        this.ConnectionAddr = peer.ConnectionAddr; // Set connection address
+    }
+
+    /**
+     * Serialize given peer to byte array.
+     * 
+     * @return 
+     */
+    public byte[] Bytes() {
+        return(SerializationUtils.serialize(this)); // Serialize
     }
 }
